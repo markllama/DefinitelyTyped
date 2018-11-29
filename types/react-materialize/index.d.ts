4f90f8a7db2e89cd67bc347de1f4f8ef3bcc8d87
Mark Lamourine <markllama@gmail.com>
2018-11-28 22:13:01 -0500

    try adding NavbarProps interface

--- types/react-materialize/index.d.ts
+++ types/react-materialize/index.d.ts
@@ -2,16 +2,16 @@
 // Project: https://github.com/react-materialize/react-materialize
 // Definitions by: Mark Lamourine <https://github.com/markllama>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyType
-// TypeScript Version: 3.0
+// TypeScript Version: 2.8
 
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
 export interface NavbarProps {
-  children: string | node ;
+  children: string ;
   divider: boolean;
   href: string;
-  onClick: any;
+  onClick(): void;
 }
 
 export class Navbar extends React.Component<NavbarProps, any> {}
