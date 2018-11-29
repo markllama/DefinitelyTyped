450e462d2b44a4213a7cb063dfa99f517ff1c91b
Mark Lamourine <markllama@gmail.com>
2018-11-28 20:22:31 -0500

    add required boilerplate

---
+++ types/react-materialize/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for react-materialize 2.5.0
+// Project: https://github.com/react-materialize/react-materialize
+// Definitions by: Mark Lamourine <https://github.com/markllama/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyType
+// TypeScript Version: 3.0
+
+export {
+  Navbar,
+  NavItem
+} from 'react-materialize'
