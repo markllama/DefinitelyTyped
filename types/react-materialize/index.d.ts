ef164658ae8a469fdb0c1d32e0b843861fa05745
Mark Lamourine <markllama@gmail.com>
2018-11-28 20:48:17 -0500

    still narrowing the settings

--- types/react-materialize/index.d.ts
+++ types/react-materialize/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for react-materialize 2.5
 // Project: https://github.com/react-materialize/react-materialize
-// Definitions by: Mark Lamourine <https://github.com/markllama/>
+// Definitions by: Mark Lamourine <https://github.com/markllama>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyType
 // TypeScript Version: 3.0
 
